c09d71d696b99f72ce8cc759a62600fde92faa85
Olya <119108499+Olyala94@users.noreply.github.com>
2023-05-07 08:17:05 +0300

    #63 Ajax İşlemleri

---
+++ TraversalCoreProje/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,49 @@
+﻿
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+using TraversalCoreProje.Models;
+
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("Admin", "travelproject05@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuider = new BodyBuilder();
+            bodyBuider.TextBody = mailRequest.Body;
+            mimeMessage.Body= bodyBuider.ToMessageBody();
+
+            mimeMessage.Subject= mailRequest.Subject;   
+
+             SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("travelproject05@gmail.com", "lqexmdxuidgvzcwr");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+
+           // mimeMessage.Body=mailRequest.Body.ToString();
+
+            return View();
+        }
+    }
+}
+//travelproject05 @gmail.com
---
+++ TraversalCoreProje/Models/CityClass.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class CityClass
+    {
+        public int CityID { get; set; }
+
+        public string CityName { get; set; }
+
+        public string CityCountry { get; set; }
+    }
+}
