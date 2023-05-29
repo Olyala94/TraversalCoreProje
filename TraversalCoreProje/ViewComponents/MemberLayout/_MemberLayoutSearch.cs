3b90f0fab938af5bfd01e3c3bcfda4545d0ba7bc
Olya <119108499+Olyala94@users.noreply.github.com>
2023-05-29 19:22:51 +0300

    #95 - Bazı Revizeler

---
+++ TraversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutSearch.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutSearch :ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); } 
+    }
+}
